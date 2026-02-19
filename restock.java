0ab1223ab611b9f9babd55049aa57ab469e1d01e
jamiejones8 <jamiedjones358@gmail.com>
2026-02-18 20:44:37 -0600

    Added javadoc to restock.java

--- restock.java
+++ restock.java
@@ -1,7 +1,12 @@
-/*
+/**
 * Restocks an item in the inventory
 * Searches for the target item name and increases its stock by one if found
 * If not found, prints "Item not found"
+*
+* @param names   Array of item names
+* @param stocks  Array of number of each item in stock
+* @param target  String that contains the name of target item
+* @param amout   Amount to add to the stock of target item
 */
 
 public static void restockItem(String[] names, int[] stocks, String target, int amount){
