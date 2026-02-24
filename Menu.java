0e077d53053ab028c5d504220ff8c17841e62d2f
Razzieeee <amk224@txstate.edu>
2026-02-24 14:22:47 -0600

    javadoc added for Menu.java

--- Menu.java
+++ Menu.java
@@ -1,5 +1,15 @@
+/**
+ * Menu class for the Grocery Management system.
+ * Responsible for displaying the main menu options to the user.
+ */
 class Menu {
 
+    /**
+     * Prints the main menu with available options for the user.
+     * Options include viewing stock, restocking items, and exiting the application.
+     * 
+     * @return void - no return value
+     */
     public void printMenu() {
         System.out.println("Grocery Management Menu");
         System.out.println("1. View Stock");
